7c6598e350093a89aa25e84cff491fc099c34037
tstrait21 <tstrait21@gmail.com>
2017-09-15 01:43:58 -0400

    First commit from IntelliJ.  Made changes to retrieveToken().  Now receiving access token.

--- src/com/tstrait21/reddit/Main.java
+++ src/com/tstrait21/reddit/Main.java
@@ -1,5 +1,9 @@
 package com.tstrait21.reddit;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -7,45 +11,46 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Base64;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class Main {
 
 	private static String ACCESS_TOKEN_URL = "https://www.reddit.com/api/v1/access_token";
-	// private static String GRANT_TYPE = "https://oauth.reddit.com/grants/installed_client";
+
+	private static String GRANT_TYPE = "https://oauth.reddit.com/grants/installed_client";
+	private static String DEVICE_ID = "DO_NOT_TRACK_THIS_DEVICE";
 
 	public static void main(String[] args) {
-		System.out.println(retrieveToken());
+		System.out.println(retrieveToken(args[0], args[1]));
 	}
 
-	private static String retrieveToken() {
+	private static String retrieveToken(String client_id, String secret) {
 		try {
-			URL obj = new URL(ACCESS_TOKEN_URL);
+			URL accessTokenUrl = new URL(ACCESS_TOKEN_URL);
 
-			HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
+			HttpsURLConnection connection = (HttpsURLConnection) accessTokenUrl.openConnection();
 
-			conn.setRequestMethod("POST");
+			connection.setRequestMethod("POST");
 
-			String userCredentials = "uaUBC9DLV3WVTA:dEYKYUl3NMwaggp5omR8F-tFkzs";
+			String userCredentials = client_id + ":" + secret;
 			String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
-			conn.setRequestProperty("Authorization", basicAuth);
 
-			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
-			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Authorization", basicAuth);
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-			String urlParams = "grant_type=https://oauth.reddit.com/grants/installed_client" + "&redirect_uri=" + "http://localhost:8080";
+			String urlParams = "grant_type=" + GRANT_TYPE
+					+ "&device_id=" + DEVICE_ID;
 
-			conn.setDoOutput(true);
-			DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+			connection.setDoOutput(true);
+			DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
 			dos.writeBytes(urlParams);
 			dos.flush();
 			dos.close();
 
-			int responseCode = conn.getResponseCode();
-			System.out.println("\nSending POST request to URL: " + obj.getPath());
-			System.out.println("Response Code: " + responseCode);
+			int responseCode = connection.getResponseCode();
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			System.out.println("Response code : " + responseCode);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
 
@@ -55,7 +60,13 @@ public class Main {
 
 			in.close();
 
-			return response.toString();
+			System.out.println(response.toString());
+
+			JsonObject jsonObj = new Gson().fromJson(response.toString(), JsonObject.class);
+
+			String access_token = jsonObj.get("access_token").getAsString();
+
+			return access_token;
 		} catch (IOException e) {
 			e.printStackTrace();
 
