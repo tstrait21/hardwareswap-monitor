c7bd831ae5acec96bcb725a85d37a153ef5ec899
Terry Strait <tstrait21@gmail.com>
2017-10-16 13:58:21 -0400

    Initial commit with Gradle
    Implemented use of Gradle, configured Log4j 2 to log to files

---
+++ src/main/java/com/tstrait21/reddit/Keywords.java
@@ -0,0 +1,34 @@
+package com.tstrait21.reddit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Keywords {
+
+    private String filepath;
+    public List<String> keywordList;
+
+    public Keywords(String filepath) {
+        this.filepath = filepath;
+        this.keywordList = getKeywordList();
+    }
+
+    private List<String> getKeywordList() {
+        try (BufferedReader br = new BufferedReader(new FileReader(this.filepath))) {
+            String currentLine;
+
+            while ((currentLine = br.readLine()) != null) {
+                this.keywordList.add(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+
+        return new ArrayList<>();
+    }
+}
