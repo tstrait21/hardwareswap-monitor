0e5c132e27fb7e3849b0b4ecf29bf2af11384996
Terry Strait <tstrait21@gmail.com>
2017-10-18 16:46:04 -0400

    Changed exception handling and how Keywords is instantiated

--- src/main/java/com/tstrait21/reddit/Main.java
+++ src/main/java/com/tstrait21/reddit/Main.java
@@ -3,21 +3,25 @@ package com.tstrait21.reddit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.URL;
+
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        RedditDAO dao = new RedditDAO(args[0], args[1]);
+        try {
+            RedditDAO dao = new RedditDAO(args[0], args[1]);
 
-        if (dao.isTokenValid()) {
-            XmlParser xmlParser = new XmlParser(dao.getListings());
+            if (dao.isTokenValid()) {
+                XmlParser xmlParser = new XmlParser(dao.getListings());
 
-            Keywords keywords = new Keywords("/resources/keywords.txt");
+                Keywords keywords = new Keywords();
 
-            xmlParser.checkForKeywords(keywords.keywordList);
-        } else {
-            logger.fatal("Error - issue retrieving access token.");
+                xmlParser.checkForKeywords(keywords.keywordList);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.fatal("Error - no credentials provided.  Please provide client_id and client_secret as parameters.");
         }
     }
 }
