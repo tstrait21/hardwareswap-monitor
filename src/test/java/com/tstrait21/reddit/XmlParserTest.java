a8029b242b173ec06307a2863b3054e28f0a5c9f
Terry Strait <tstrait21@gmail.com>
2017-10-20 12:31:03 -0400

    Renamed initial unit test and created one for keyword matching

--- src/test/java/com/tstrait21/reddit/XmlParserTest.java
+++ src/test/java/com/tstrait21/reddit/XmlParserTest.java
@@ -6,17 +6,38 @@ import org.junit.Test;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XmlParserTest {
 
     @Test
-    public void testCheckForListings() {
+    public void testXmlParserInitialization() {
         try {
             String xml = new String(Files.readAllBytes(Paths.get(new File(this.getClass().getClassLoader().getResource("sample.xml").getFile()).getPath())));
 
             XmlParser xmlParser = new XmlParser(xml);
 
             Assert.assertNotNull(xmlParser);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCheckForKeywords() {
+        try {
+            String xml = new String(Files.readAllBytes(Paths.get(new File(this.getClass().getClassLoader().getResource("sample.xml").getFile()).getPath())));
+
+            XmlParser xmlParser = new XmlParser(xml);
+
+            List<String> keywordList = new ArrayList<>();
+
+            keywordList.add("USA-PA");
+            keywordList.add("GTX");
+            keywordList.add("Corsair");
+
+            xmlParser.checkForKeywords(keywordList);
         } catch (IOException e) {
             e.printStackTrace();
         }
